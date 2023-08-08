190de7b4aa4559de4f366777b5a2a44d5e6d5c45
Dankotchev <80976020+Dankotchev@users.noreply.github.com>
2023-08-08 17:38:12 -0300

    Sala de Aula - 2308.8 -- Interpolação, event binding e diretivas estruturais

--- FP3C6 - 01 Aulas Iniciais/03 Ionic/01aula-inicial/src/app/home/home.page.ts
+++ FP3C6 - 01 Aulas Iniciais/03 Ionic/01aula-inicial/src/app/home/home.page.ts
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  // Atributos
+  public nome: string = "Danilo Quirino";
+  public show: boolean = true;
+  public nomes: string[] = ["Danilo", "Daniele", "Giovana", "Samara"];
+
+  // Construtor
   constructor() {}
 
+  // Métodos
+  public conveterMaiusculo(): string {
+    return this.nome.toLocaleUpperCase();
+  }
+
+  public exibir() {
+    console.log(this.conveterMaiusculo());
+  }
+
+  public alterarShow(){
+    this.show = !this.show;
+  }
+
+  public excluir( indice: number){
+    let valor = this.nomes.splice(indice, 1);
+    console.log(`${valor} faltou!`);
+  }
+
+  public adicionar(){
+    this.nomes.unshift(this.nome);
+  }
 }
