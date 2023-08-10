2db48489d1306a758d998309c0262943f3e825e1
Dankotchev <80976020+Dankotchev@users.noreply.github.com>
2023-08-10 15:39:57 -0300

    Sala de Aula 2308.10 -- Grid e Toast

--- FP3C6 - 01 Aulas Iniciais/03 Ionic/01aula-inicial/src/app/home/home.page.ts
+++ FP3C6 - 01 Aulas Iniciais/03 Ionic/01aula-inicial/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,14 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   // Atributos
-  public nome: string = "Danilo Quirino";
+  public nome: string = 'Danilo Quirino';
+  public salario: number = 1000.0;
   public show: boolean = true;
-  public nomes: string[] = ["Danilo", "Daniele", "Giovana", "Samara"];
+  public nomes: String[] = ['Danilo', 'Daniele', 'Giovana', 'Samara'];
+  public listaPessoas: any[] = [];
 
   // Construtor
-  constructor() {}
+  constructor(private toastController: ToastController) {}
 
   // Métodos
   public conveterMaiusculo(): string {
@@ -24,16 +26,44 @@ export class HomePage {
     console.log(this.conveterMaiusculo());
   }
 
-  public alterarShow(){
+  public alterarShow() {
     this.show = !this.show;
   }
 
-  public excluir( indice: number){
+  public excluirLista(indice: number) {
     let valor = this.nomes.splice(indice, 1);
     console.log(`${valor} faltou!`);
   }
 
-  public adicionar(){
-    this.nomes.unshift(this.nome);
+  public excluirPessoa(indice: number) {
+    let valor = this.listaPessoas.splice(indice, 1);
+    console.log(valor);
+  }
+
+  public adicionar() {
+    if (this.nome.length < 3) {
+      this.exibirMensagem('Nome deve conter mais de 3 caracteres');
+    } else if (this.salario <= 0) {
+      this.exibirMensagem('Salário deve ser maior que 0');
+    } else {
+      const pessoa = {
+        nome: this.nome,
+        salario: this.salario,
+      };
+      this.listaPessoas.unshift(pessoa);
+      this.exibirMensagem('Pessoa adicionada');
+    }
+  }
+
+  //  Exibir mensagem utilizando Toast
+  //  'top' | 'middle' | 'bottom' 3 parameros únicos possiveis para a posição
+  async exibirMensagem(mensagem: string) {
+    const toast = await this.toastController.create({
+      message: mensagem,
+      duration: 550,
+      position: 'bottom',
+    });
+
+    await toast.present();
   }
 }
