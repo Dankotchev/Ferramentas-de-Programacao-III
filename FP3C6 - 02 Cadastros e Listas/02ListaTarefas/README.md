8f04c510213d25399afea94b00e3e973e22686f4
Danilo D. Quirino <80976020+Dankotchev@users.noreply.github.com>
2023-08-22 17:41:46 -0300

    Lista de Tarefas -- Inclusão, exclusão, modificação de status

---
+++ FP3C6 - 02 Cadastros e Listas/02ListaTarefas/README.md
@@ -0,0 +1,9 @@
+# Aplicativo de Tarefas
+Crie um aplicativo para cadastro, listagem, exclusão e alteração de status de Tarefas. Uma tarefa tem, no mínimo, a descrição e o status. Implementar as seguintes validações:  
+
+1.  Ao cadastrar uma tarefa, o status inicial é Cadastrada e a descrição deve ter pelo menos  3 caracteres.    
+2.  Não pode existir tarefas com a mesma descrição;
+3.  O status pode ser modificado para Cancelada ou Finalizada. Uma tarefa com o status Cancelada não pode ser Finalizada.
+
+
+
--- FP3C6 - 02 Cadastros e Listas/02ListaTarefas/src/app/home/home.page.ts
+++ FP3C6 - 02 Cadastros e Listas/02ListaTarefas/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,78 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // Atributos
+  public listaTarefas: any[] = [];
+  public tarefa: any = {};
 
-  constructor() {}
+  // Construtor
+  constructor(private toastController: ToastController) { }
 
+  // Métodos
+  public showComandos(posicao: number): boolean {
+    if (this.listaTarefas[posicao].status === "Cadastrada")
+      return true;
+    else
+      return false;
+  }
+
+  public adicionar() {
+    console.log(this.tarefa.descricao);
+    if (this.tarefa.descricao == undefined || this.tarefa.descricao.length < 3) {
+      this.exibirMensagem('Nome deve conter mais de 3 caracteres');
+      return;
+    } 
+    if (this.listaTarefas.find(tarefa => tarefa.descricao === this.tarefa.descricao) != undefined) {
+      this.exibirMensagem('Tarefa com descrição já adicionada.');
+      return;
+    }
+
+    console.log(this.listaTarefas);
+    this.tarefa.status = "Cadastrada";
+    this.listaTarefas.unshift(this.tarefa);
+    this.ordenarTarefas();
+    this.exibirMensagem('Tarefa adicionada');
+    this.tarefa = {};
+  }
+
+  public excluir(posicao: number) {
+    this.listaTarefas.splice(posicao,1);
+    this.ordenarTarefas();
+  }
+
+  public finalizar(posicao: number) {
+    this.listaTarefas[posicao].status = "Finalizada";
+    this.ordenarTarefas();
+  }
+
+  public cancelar(posicao: number) {
+    this.listaTarefas[posicao].status = "Cancelada";
+    this.ordenarTarefas();
+  }
+
+  async exibirMensagem(mensagem: string) {
+    const toast = await this.toastController.create({
+      message: mensagem,
+      duration: 550,
+      position: 'bottom',
+    });
+
+    await toast.present();
+  }
+
+  ordenarTarefas() {
+    this.listaTarefas.sort((a, b) => {
+      if (a.status === b.status) {
+        return 0;
+      } else if (a.status === "Cadastrada") {
+        return -1;
+      } else if (b.status === "Cadastrada") {
+        return 1;
+      } else if (a.status === "Finalizada") {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+  }
 }
