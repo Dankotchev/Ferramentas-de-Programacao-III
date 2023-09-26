136f00aa8cf389ec9e05c209cfb7fbfd6fbaca88
Dankotchev <80976020+Dankotchev@users.noreply.github.com>
2023-09-26 17:29:29 -0300

    Sala de Aula - 2309.26 -- Post e Image. Inclusão do modelo, migrations e data-sources

--- FP3C6 - 04 Back-end/01backend/src/data-source.ts
+++ FP3C6 - 04 Back-end/01backend/src/data-source.ts
@@ -1,18 +1,31 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
-import User from "./models/User";
-import Product from "./models/Product";
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+
+import { createUser1695154116749 } from './database/migrations/1695154116749-create_user';
+import { createProduct1695755545038 } from './database/migrations/1695755545038-create_product';
+import { CreatePostTable1695759541556 } from './database/migrations/1695759541556-CreatePostTable';
+import { CreateImageTable1695759550415 } from './database/migrations/1695759550415-CreateImageTable';
+
+import User from './models/User';
+import Product from './models/Product';
+import Post from './models/Post';
+import Image from './models/Image';
 
 export const AppDataSource = new DataSource({
-  type: "mysql",
-  host: "localhost",
+  type: 'mysql',
+  host: 'localhost',
   port: 3306,
-  username: "root",
-  password: "ifsp",
-  database: "db_aula",
+  username: 'root',
+  password: 'ifsp',
+  database: 'db_aula',
   synchronize: false,
   logging: false,
-  entities: [User, Product],
-  migrations: [],
+  entities: [User, Product, Post, Image],
+  migrations: [
+    createUser1695154116749,
+    createProduct1695755545038,
+    CreatePostTable1695759541556,
+    CreateImageTable1695759550415,
+  ],
   subscribers: [],
 });
---
+++ FP3C6 - 04 Back-end/01backend/src/database/migrations/1695759541556-CreatePostTable.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreatePostTable1695759541556 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'post',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            unsigned: true,
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'message',
+            type: 'varchar',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('post');
+  }
+}
