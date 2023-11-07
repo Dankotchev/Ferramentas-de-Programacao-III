7c551d5aec98fc471675cf23d53fb9952c825f6f
Danilo D. Quirino <80976020+Dankotchev@users.noreply.github.com>
2023-11-07 16:46:16 -0300

    Ajustes nas rotas

--- FP3C6 - 00 Atividades Avaliativas/02AtividadePratica/src/routes.ts
+++ FP3C6 - 00 Atividades Avaliativas/02AtividadePratica/src/routes.ts
@@ -3,27 +3,12 @@ import CursoController from './controller/CursoController';
 import Disciplina from './models/Disciplina';
 import DisciplinaController from './controller/DisciplinaController';
 
-
-
 const routes = Router();
 
-
-/**
- * Rotas para os produtos
- */
-// routes.post('/products', ProductsController.create);
-// routes.get('/products', ProductsController.index);
-// routes.get('/products/:codigo', ProductsController.show);
-// routes.get("/products/name/:name", ProductsController.findByName);
-// routes.get("/products/price/:price", ProductsController.findByPrice);
-// routes.delete('/products/:codigo', ProductsController.delete);
-// routes.put('/products/:codigo', ProductsController.update);
-
-routes.get('/curso/nivel/:nivel', CursoController.findByNivel)
-routes.get('/curso/nome/:nome', CursoController.findByNome)
-routes.get('/disciplinas/:nome', DisciplinaController.findByNome)
-routes.delete('/curso/:codigo', CursoController.delete)
-routes.post('/disciplinas', DisciplinaController.create)
-
+routes.get('/curso/nivel/:nivel', CursoController.findByNivel);
+routes.get('/curso/nome/:nome', CursoController.findByNome);
+routes.get('/disciplinas/:nome', DisciplinaController.findByNome);
+routes.delete('/curso/:codigo', CursoController.delete);
+routes.post('/disciplinas', DisciplinaController.create);
 
 export default routes;
