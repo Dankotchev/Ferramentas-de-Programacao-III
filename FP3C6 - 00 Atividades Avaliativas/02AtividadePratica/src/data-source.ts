3e32fc0d967b6ed513634c1ec263a74f9c7782a0
Danilo D. Quirino <80976020+Dankotchev@users.noreply.github.com>
2023-11-28 16:25:24 -0300

    Sala de Aula - 2311.28 -- update data-source

--- FP3C6 - 00 Atividades Avaliativas/02AtividadePratica/src/data-source.ts
+++ FP3C6 - 00 Atividades Avaliativas/02AtividadePratica/src/data-source.ts
@@ -3,9 +3,12 @@ import { DataSource } from 'typeorm';
 import Nivel from './models/Nivel';
 import Curso from './models/Curso';
 import Disciplina from './models/Disciplina';
+import Usuario from './models/Usuario';
+
 import { CreateNivelTable1698779915484 } from './database/migrations/1698779915484-CreateNivelTable';
 import { CreateCursoTable1698779925909 } from './database/migrations/1698779925909-CreateCursoTable';
 import { CreateDisciplinaTable1698779939189 } from './database/migrations/1698779939189-CreateDisciplinaTable';
+import { CreateUsuarioTable1701199291156 } from './database/migrations/1701199291156-CreateUsuarioTable';
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
@@ -17,11 +20,12 @@ export const AppDataSource = new DataSource({
   migrationsRun: true,
   synchronize: false,
   logging: false,
-  entities: [Nivel, Curso, Disciplina],
+  entities: [Nivel, Curso, Disciplina, Usuario],
   migrations: [
     CreateNivelTable1698779915484,
     CreateCursoTable1698779925909,
     CreateDisciplinaTable1698779939189,
+    CreateUsuarioTable1701199291156
   ],
   subscribers: [],
 });
