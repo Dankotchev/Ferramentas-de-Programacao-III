a4e45d73c9fd184fb330c3a1d9fbb366bd2bb26a
Danilo D. Quirino <80976020+Dankotchev@users.noreply.github.com>
2023-11-30 15:37:19 -0300

    Sala de Aula - 2311.28 -- Iniciando um frontend

---
+++ FP3C6 - 00 Atividades Avaliativas/02AtividadePraticaApp/src/app/login/login.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  constructor(
+    private authentication: AuthenticationService,
+    private router: Router
+  ) {}
+  ngOnInit(): void {}
+
+  public email: string = '';
+  public senha: string = '';
+
+  async login() {
+    const usuario = {
+      email: this.email,
+      senha: this.senha,
+    };
+    const retorno = await this.authentication.login(usuario);
+
+    if (retorno) {
+      console.log('Login OK');
+      this.router.navigate(["/home"]);
+    } else {
+      console.log('Login failed.');
+    }
+  }
+}
